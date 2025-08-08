cddfd11ba5839a1b674289b30f680f4b67c648fd
LendyAbel <lendyabel93@gmail.com>
2025-08-08 22:36:12 +0200

    7.8: Ultimate Hooks

--- ultimate-hooks/src/App.jsx
+++ ultimate-hooks/src/App.jsx
@@ -1,36 +1,43 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const useField = (type) => {
+const useField = type => {
   const [value, setValue] = useState('')
 
-  const onChange = (event) => {
+  const onChange = event => {
     setValue(event.target.value)
   }
 
   return {
     type,
     value,
-    onChange
+    onChange,
   }
 }
 
-const useResource = (baseUrl) => {
+const useResource = baseUrl => {
   const [resources, setResources] = useState([])
 
   // ...
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await axios.get(baseUrl)
+      setResources(response.data)
+    }
+    fetch()
+  }, [baseUrl])
 
-  const create = (resource) => {
+  const create = async resource => {
     // ...
+    const response = await axios.post(baseUrl, resource)
+    setResources(prev => prev.concat(response.data))
   }
 
   const service = {
-    create
+    create,
   }
 
-  return [
-    resources, service
-  ]
+  return [resources, service]
 }
 
 const App = () => {
@@ -41,14 +48,14 @@ const App = () => {
   const [notes, noteService] = useResource('http://localhost:3005/notes')
   const [persons, personService] = useResource('http://localhost:3005/persons')
 
-  const handleNoteSubmit = (event) => {
+  const handleNoteSubmit = event => {
     event.preventDefault()
     noteService.create({ content: content.value })
   }
- 
-  const handlePersonSubmit = (event) => {
+
+  const handlePersonSubmit = event => {
     event.preventDefault()
-    personService.create({ name: name.value, number: number.value})
+    personService.create({ name: name.value, number: number.value })
   }
 
   return (
@@ -58,15 +65,21 @@ const App = () => {
         <input {...content} />
         <button>create</button>
       </form>
-      {notes.map(n => <p key={n.id}>{n.content}</p>)}
+      {notes.map(n => (
+        <p key={n.id}>{n.content}</p>
+      ))}
 
       <h2>persons</h2>
       <form onSubmit={handlePersonSubmit}>
-        name <input {...name} /> <br/>
+        name <input {...name} /> <br />
         number <input {...number} />
         <button>create</button>
       </form>
-      {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
+      {persons.map(n => (
+        <p key={n.id}>
+          {n.name} {n.number}
+        </p>
+      ))}
     </div>
   )
 }
